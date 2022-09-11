9099959cb34e6d517b93655386041093836b2f75
gpradinett <gpradinett@gmail.com>
2022-09-11 03:47:16 -0500

    ✅initial reamde

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application:
+- Your web application must be listening on 0.0.0.0, port 5000
+- Routes:
+* /: display “Hello HBNB!”
+* /hbnb: display “HBNB”
+- You must use the option strict_slashes=False in your route definition
+"""
+
+from flask import Flask
+
+# Creando una instancia de flask con el nombre del archivo nuestro
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Function hello, that return a prompt saying: Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display “HBNB”"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Function, that return a prompt saying: C and text passed"""
+    replace = text.replace("_", " ")
+    return "C {}".format(replace)
+
+
+if __name__ == '__main__':
+    """
+    Set host IP addres and port
+    Info: https://www.codegrepper.com/code-examples/python/flask+set+listen+
+    address+port
+    """
+    app.run(host="0.0.0.0", port=5000)
