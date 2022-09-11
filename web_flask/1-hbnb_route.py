17ca6d3598e0630d4f76de08ec82de0a32fb77eb
gpradinett <gpradinett@gmail.com>
2022-09-11 03:33:10 -0500

    ✅initial reamde

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -19,10 +19,12 @@ def hello_HBNB():
     """Function hello, that return a prompt saying: Hello HBNB!"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """display “HBNB”"""
     return "HBNB"
+
 
 if __name__ == '__main__':
     """
