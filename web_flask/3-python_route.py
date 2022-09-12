b9fae7727d3df0e531581e94313cea004741a703
gpradinett <gpradinett@gmail.com>
2022-09-11 19:17:16 -0500

    ✅ task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application:
+- Your web application must be listening on 0.0.0.0, port 5000
+- Routes:
+* /: display “Hello HBNB!”
+* /hbnb: display “HBNB”
+* /c/<text>: display “C ”, followed by the value of the text variable (replace
+underscore _ symbols with a space )
+* /python/(<text>): display “Python ”, followed by the value of the text
+variable (replace underscore _ symbols with a space )
+- The default value of text is “is cool”
+- You must use the option strict_slashes=False in your route definition
+"""
+
+from flask import Flask
+
+# Creando una instancia de flask con el nombre del archivo nuestro
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Function hello, that return a prompt saying: Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display “HBNB”"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Function, that return a prompt saying: C and text passed"""
+    replace = text.replace("_", " ")
+    return "C {}".format(replace)
+
+
+@app.route("/python", defaults={'text': "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """
+    Function, that return a prompt tha display a message
+    Info default varaibles:
+    https://stackoverflow.com/questions/14032066/can-flask-have-optional-url
+    -parameters
+    """
+    replace = text.replace("_", " ")
+    return "Python {}".format(replace)
+
+
+if __name__ == '__main__':
+    """
+    Set host IP addres and port
+    Info: https://www.codegrepper.com/code-examples/python/flask+set+listen+
+    address+port
+    """
+    app.run(host="0.0.0.0", port=5000)
